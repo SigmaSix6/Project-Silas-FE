9fa3d5f37c2bbebf8d24d062b6898b78d41ee2e7
SigmaSix6 <SigmaSix6@users.noreply.github.com>
2023-04-03 15:42:48 +0700

    Testing Boilerplate

---
+++ src/components/test.js
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+const Test = (props) => {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+};
+
+export default Test;
